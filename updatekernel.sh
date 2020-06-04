75c647644f67c386a40ee8a7c6e77536eb0d3db9
TotalChen <totalatot@gmail.com>
2020-06-05 00:09:38 +0800

    add updatekernel.sh

---
+++ updatekernel.sh
@@ -0,0 +1,10 @@
+chromium https://kernel.ubuntu.com/~kernel-ppa/mainline/
+#wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.3/linux-headers-5.6.3-050603_5.6.3-050603.202004080837_all.deb
+#wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.3/linux-headers-5.6.3-050603-generic_5.6.3-050603.202004080837_amd64.deb
+#wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.3/linux-headers-5.6.3-050603-lowlatency_5.6.3-050603.202004080837_amd64.deb
+#wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.3/linux-image-unsigned-5.6.3-050603-generic_5.6.3-050603.202004080837_amd64.deb
+#wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.3/linux-image-unsigned-5.6.3-050603-lowlatency_5.6.3-050603.202004080837_amd64.deb
+#wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.3/linux-modules-5.6.3-050603-generic_5.6.3-050603.202004080837_amd64.deb
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.3/linux-modules-5.6.3-050603-lowlatency_5.6.3-050603.202004080837_amd64.deb
+#sudo dpkg -i *.deb
+#uname -sr
